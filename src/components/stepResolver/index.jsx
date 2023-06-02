31d711a3ed3eae6f1a0fe06c475dba3ef92762fc
Carmen Santiago <greminoficial@gmail.com>
2023-06-01 18:18:23 -0600

    feat: step indicator on mobile view

--- src/components/stepResolver/index.jsx
+++ src/components/stepResolver/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useCompletionContext } from '../../context/completion';
 import { BsClockFill } from 'react-icons/bs';
-import StepListIndicator from './stepIndicator';
+import { MobileStepListIndicator, StepListIndicator } from './stepIndicator';
 import { getFlujoStatus } from '../../domain/steps';
 import StepsManifest from '../../domain/steps/manifest';
 import { toast } from 'react-hot-toast';
@@ -18,7 +18,7 @@ function formatTime(seconds) {
 }
 
 function StepResolver() {
-    const [customCurrentStep, setCurrStep] = React.useState(null);
+    const [, setCurrStep] = React.useState(null);
 
     const { state: { flujo, secondsLeft: _secLeft }, actions } = useCompletionContext();
     const [secondsLeft, setSecondsLeft] = React.useState(_secLeft);
@@ -70,6 +70,15 @@ function StepResolver() {
         />
     ), [clickIndicator, currStep, completedSteps]);
 
+    const IndicatorHorizontal = React.useMemo(() => () => (
+        <MobileStepListIndicator
+            steps={applicableSteps}
+            completedSteps={completedSteps}
+            onClickIndicator={clickIndicator}
+            currStep={currStep}
+        />
+    ), [clickIndicator, currStep, completedSteps]);
+
     const StepComponent = React.useCallback(() => {
         const C = currStep.component;
         return (
@@ -79,11 +88,14 @@ function StepResolver() {
 
 
     return (
-        <div className="flex-1 flex flex-row w-full mt-10 mx-1 sm:mx-10">
+        <div className="flex-1 flex flex-col sm:flex-row w-full mt-2 sm:mt-10 mx-1 sm:mx-10">
             <div className="w-1/3 pt-5 pr-5 justify-center hidden sm:flex">
                 <Indicator />
             </div>
-            <div className="max-h-fit w-full">
+            <div className="flex sm:hidden w-full justify-center">
+                <IndicatorHorizontal />
+            </div>
+            <div className="max-h-fit pt-3 sm:pt-0 w-full">
                 <div className="flex items-center m-1 text-montserrat font-semibold text-gray-500 text-sm">
                     <BsClockFill /><p className="ml-1"> {formatTime(secondsLeft)} left</p>
                 </div>
--- src/components/stepResolver/stepIndicator.jsx
+++ src/components/stepResolver/stepIndicator.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { FLujoDoneIcon } from '../icons/icon.map';
 
 const UnionLine = ({ remark }) => (
@@ -7,8 +7,14 @@ const UnionLine = ({ remark }) => (
   </div>
 );
 
-const StepIndicator = ({ icon, onClick, completed, active, drawNextIndicator, text }) => {
+const UnionLineHorizontal = ({ remark }) => (
+  <div className="w-full items-center justify-start flex">
+    <div className={`h-1 w-full ${remark ? "bg-blue-700" : "bg-white"}`}></div>
+  </div>
+);
 
+
+const Step = ({ icon, onClick, completed, active, drawNextIndicator, text }) => {
   const activeClasses = active ? "border-blue-600" : "border-transparent";
   const completedClasses = completed ? "bg-blue-600 text-white" : "bg-white";
   const textActiveClasses = active ? "font-bold text-gray-700" : "font-semibold";
@@ -31,24 +37,64 @@ const StepIndicator = ({ icon, onClick, completed, active, drawNextIndicator, te
   );
 }
 
-export default function StepListIndicator(props) {
-  const { steps, currStep, onClickIndicator, completedSteps } = props;
-
-  const components = useMemo(() => steps.map(function (s, index) {
-    const active = currStep.key === s.key;
-    const completed = completedSteps.includes(s.key);
-    const nextExists = index < steps.length - 1;
-
-    const handleStepClick = () => onClickIndicator(s.key);
-
-    return <StepIndicator key={s.key} onClick={handleStepClick} text={s.title} icon={s.icon} active={active} completed={completed} drawNextIndicator={nextExists} />;
-  }), [steps, onClickIndicator, currStep, completedSteps]);
+const StepHorizontal = ({ icon, onClick, completed, active, drawNextIndicator, text }) => {
+  const activeClasses = active ? "border-blue-600" : "border-transparent";
+  const completedClasses = completed ? "bg-blue-600 text-white" : "bg-white";
+  const Icon = icon;
 
   return (
-    <div>
-      {components}
+    <div className="flex w-full">
+      <div className="">
+        <div onClick={onClick}
+          className={`p-2 cursor-pointer border rounded-lg text-gray-700 relative ${activeClasses} ${completedClasses}`}
+        >
+          {completed && <div className="absolute top-0 right-0 translate-x-1 -translate-y-1 z-10 text-green-600"><FLujoDoneIcon /></div>}
+          <Icon />
+        </div>
+      </div>
+      {drawNextIndicator && <UnionLineHorizontal remark={completed} />}
     </div>
   );
 }
 
+export function StepListIndicator(props) {
+  const { steps, currStep, onClickIndicator, completedSteps } = props;
 
+  return (
+    <div className="flex flex-row sm:flex-col">
+      {
+        steps.map((s, index) => {
+          const active = currStep.key === s.key;
+          const completed = completedSteps.includes(s.key);
+          const nextExists = index < steps.length - 1;
+
+          const handleStepClick = () => onClickIndicator(s.key);
+
+          return <Step key={s.key} onClick={handleStepClick} text={s.title} icon={s.icon} active={active} completed={completed} drawNextIndicator={nextExists} />;
+        })
+      }
+    </div>
+  );
+}
+
+export function MobileStepListIndicator(props) {
+  const { steps, currStep, onClickIndicator, completedSteps } = props;
+  return (
+    <div className="flex flex-col w-full">
+      <p className={`pl-1 select-none cursor-pointer text-lg font-bold text-gray-700`}>{currStep.title}</p>
+      <div className="flex pt-3">
+        {
+          steps.map((s, index) => {
+            const active = currStep.key === s.key;
+            const completed = completedSteps.includes(s.key);
+            const nextExists = index < steps.length - 1;
+
+            const handleStepClick = () => onClickIndicator(s.key);
+
+            return <StepHorizontal key={s.key} onClick={handleStepClick} text={s.title} icon={s.icon} active={active} completed={completed} drawNextIndicator={nextExists} />;
+          })
+        }
+      </div>
+    </div>
+  );
+}
