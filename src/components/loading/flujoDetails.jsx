a1bb8d701bf61ccc6dbcf2e5f9c86292e29dc272
Carmen Santiago <greminoficial@gmail.com>
2023-06-04 21:25:52 -0600

    feat: skeleton loader for details view

--- src/components/loading/flujoDetails.jsx
+++ src/components/loading/flujoDetails.jsx
@@ -1,11 +1,39 @@
+import React from "react";
+
+const DetailsCard = React.memo(() => (
+    <div className="bg-white h-52 w-full shadow-sm rounded-md border p-3 animate-pulse">
+        <div class="h-3 bg-slate-200 rounded-full mb-2.5"></div>
+        <div class="h-2 bg-slate-200 rounded-full  max-w-[360px] mb-2.5"></div>
+        <div class="h-2 bg-slate-200 rounded-full  max-w-[360px] mb-2.5"></div>
+        <div class="h-2 bg-slate-200 rounded-full  max-w-[260px] mb-2.5"></div>
+        <div class="h-5"></div>
+        <div className="flex gap-2">
+            <div class="h-6 bg-slate-200 rounded-full w-6 mb-2.5"></div>
+            <div class="h-6 bg-slate-200 rounded-full w-6 mb-2.5"></div>
+            <div class="h-6 bg-slate-200 rounded-full w-6 mb-2.5"></div>
+        </div>
+    </div>
+));
+
+const StepCard = React.memo(() => (
+    <div className="mt-5">
+        <div class="ml-1 h-3 bg-slate-300 rounded-full max-w-[260px] mb-2.5 animate-pulse"></div>
+        <div className="bg-white h-36 w-full shadow-sm rounded-md border p-3 animate-pulse">
+            <div class="h-2 bg-slate-200 rounded-full  max-w-[360px] mt-2.5"></div>
+            <div class="h-2 bg-slate-200 rounded-full  max-w-[360px] mt-2.5"></div>
+        </div>
+    </div>
+));
 
 const FlujoDetailsLoadingView = () => {
     return (
-        <div className="w-full h-64 bg-slate-300 rounded-lg animate-pulse p-4">
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 max-w-[440px] mb-2.5"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 max-w-[460px] mb-2.5"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-400 max-w-[360px]"></div>
+        <div>
+            <DetailsCard />
+            <div className="flex flex-col gap-3">
+                <StepCard />
+                <StepCard />
+                <StepCard />
+            </div>
         </div>
     );
 }
